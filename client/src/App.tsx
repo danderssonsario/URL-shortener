2ae809b411348c8aa018818537f580325531348f
Daniel Andersson <da222xg@student.lnu.se>
2023-01-23 13:20:49 +0100

    Set up tailwindcss.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <div className="App">
-      
+      <h1 className="text-3xl font-bold underline flex justify-center">
+      Hello world!
+    </h1>
     </div>
   )
 }
